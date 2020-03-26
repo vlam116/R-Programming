aed5b685f7343b61d1282c18ecd2352a087d33ae
vlam116 <vlam116@gmail.com>
2020-03-26 13:18:39 -0700

    added code to return best hospital by state for heart attack and heart failure

--- ProgramAssignment3/best.R
+++ ProgramAssignment3/best.R
@@ -1,6 +1,6 @@
 ## Course 2 Week 4 Assignment - Hospital Data
+setwd("C:/Users/Lam/Desktop/coursera/datasciencecoursera/ProgramAssignment3")
 
-setwd("C:/Users/Lam/Desktop/coursera/ProgramAssignment3")
 library(tidyverse)
 
 best = function(state, outcome){
@@ -10,7 +10,10 @@ best = function(state, outcome){
           MRHA = Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,
           MRHF = Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,
           MRP = Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
-      compact = compact %>% mutate(Hospital.Name = as.character(Hospital.Name))
+      compact = compact %>% mutate(Hospital.Name = as.character(Hospital.Name),
+                                   MRHA = as.numeric(MRHA),
+                                   MRHF = as.numeric(MRHF),
+                                   MRP = as.numeric(MRP))
       levState = c(levels(compact$State))
       levOutcomes = c("heart attack","heart failure","pneumonia")
       validState = sum(state == levState)
@@ -22,7 +25,20 @@ best = function(state, outcome){
                 stop("invalid outcome")
           }
       if(outcome == "heart attack"){
-          minimum = min(tapply(compact$MRHA, compact$State, min))
+          subsetHA = compact[compact$State == state,]
+          minHA = min(subsetHA$MRHA)
+          bestHospitalHA = subsetHA[which(subsetHA$MRHA==minHA),]
+          hospitalNameHA = bestHospitalHA$Hospital.Name
+          tiedHA = sort(hospitalNameHA)
+          print(tiedHA)
+      }
+      if(outcome == "heart failure"){
+         subsetHF = compact[compact$State == state,]
+         minHF = min(subsetHF$MRHF)
+         bestHospitalHF = subsetHF[which(subsetHF$MRHF==minHF),]
+         hospitalNameHF = bestHospitalHF$Hospital.Name
+         tiedHF = sort(hospitalNameHF)
+         print(tiedHF)
       }
 }
 
